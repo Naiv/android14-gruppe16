972aa52cb9a2e167e99b1fc56f53b79c051a637e
Leakim1991 <mikael-91@hotmail.com>
2014-03-26 05:22:16 +0100

    skrevet om database og lagt til menubar

--- app/src/main/java/com/gruppe16/bensinkalkulator/MainActivity.java
+++ app/src/main/java/com/gruppe16/bensinkalkulator/MainActivity.java
@@ -6,33 +6,31 @@ import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
         final ActionBar actionBar = getActionBar();
-
-        // Specify that tabs should be displayed in the action bar.
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
-        // Create a tab listener that is called when the user changes tabs.
         ActionBar.TabListener tabListener = new ActionBar.TabListener() {
             public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
                 // show the given tab
                 Fragment fr;
                 FragmentManager fm = getFragmentManager();
                 FragmentTransaction fragmentTransaction = fm.beginTransaction();
-                if (tab.getPosition()==0){
+                if (tab.getPosition() == 0) {
                     fr = new FragmentMain();
                     fragmentTransaction.replace(R.id.myFrag, fr);
                     fragmentTransaction.commit();
-                }
-                else{
+                } else {
                     fr = new FragmentLog();
                     fragmentTransaction.replace(R.id.myFrag, fr);
                     fragmentTransaction.commit();
@@ -40,30 +38,40 @@ public class MainActivity extends Activity {
             }
 
             public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
-                // hide the given tab
             }
 
             public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
-                // probably ignore this event
             }
         };
-
-        // Add 3 tabs, specifying the tab's text and TabListener
         actionBar.addTab(actionBar.newTab().setText("Drive").setTabListener(tabListener));
         actionBar.addTab(actionBar.newTab().setText("Log").setTabListener(tabListener));
-	}
-    public void addDummy(View view) {
-        TripsDataSource datasource;
-        datasource = new TripsDataSource(this);
-        datasource.open();
-        @SuppressWarnings("unchecked")
-        String trip = new String("23.08.1991 kl 14:02 - 23km - 202kr");
-        datasource.createTrip(trip);
     }
-    public void removeAll(View view) {
-        TripsDataSource datasource;
-        datasource = new TripsDataSource(this);
-        datasource.open();
-        datasource.deleteAll();
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.emptyLog) {
+            FragmentLog.datasource = new TripsDataSource(this);
+            FragmentLog.datasource.open();
+            FragmentLog.datasource.deleteAll();
+            FragmentLog.datasource.close();
+            FragmentLog.adapter.clear();
+            FragmentLog.adapter.notifyDataSetChanged();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void addDummy(View view) {
+        FragmentLog.datasource = new TripsDataSource(this);
+        FragmentLog.datasource.open();
+        FragmentLog.datasource.createTrip("23.08.1991 kl 14:01", 202, 23);
+        FragmentLog.datasource.close();
     }
 }
